ff4635d1365e390d815b2b12ca2cef855a22c04a
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-05-22 20:48:57 +0600

    sort

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -3,18 +3,21 @@ import ServiceCard from './ServiceCard';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [asc, setAsc] = useState(true)
 
     useEffect(() => {
-        fetch('https://car-doctor-server-jwt-71.vercel.app/services')
+        fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'dsc'}`)
             .then(res => res.json())
             .then(data => setServices(data));
-    }, [])
+    }, [asc])
     return (
         <div className="mt-4">
             <div className="text-center">
                 <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
                 <h2 className="text-5xl">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+                <button className='btn '
+                onClick={() =>{setAsc(!asc)}} >{asc ? 'Price H to L' : 'Price L to H'}</button>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
